efc8953e2095ac01109857578dfbe3854143e59e
Ahmed Nageh <ahmdnajkh71@gmail.com>
2026-05-23 21:34:34 +0300

    Create leagues details screen with compositional layout extensions, dynamic glassmorphism cards, and circular teams section.

---
+++ Whistle/Features/LeaguesDetails/view/LatestResultsCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  LatestResultsCollectionViewCell.swift
+//  Whistle
+//
+//  Created by Ahmed Nageh on 23/05/2026.
+//
+
+import UIKit
+
+class LatestResultsCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Whistle/Features/LeaguesDetails/view/UpcomingEventCollectionViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  UpcomingEventCollectionViewCell.swift
+//  Whistle
+//
+//  Created by Ahmed Nageh on 22/05/2026.
+//
+
+import UIKit
+
+class UpcomingEventCollectionViewCell: UICollectionViewCell {
+    
+}

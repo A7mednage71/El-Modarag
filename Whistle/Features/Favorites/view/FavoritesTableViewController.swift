96daffd561798d0b03521167e6929c966e56dcbe
DevOmarAmer <omahar1907@gmail.com>
2026-05-31 03:04:11 +0300

    feat(favorites): implement favorites list screen with MVP architecture
    Created FavoritesTableViewController and connected it with FavoritesPresenter using loose protocols.
    
    Implemented custom FavoritesTableViewCell with a Card Overlay layout in Storyboard.
    
    Configured structural constraints and custom layoutSubviews to ensure precise cell padding and dynamic circular corner radius.
    
    Added native trailing swipe-to-delete action synced perfectly with the presenter's data source.

--- Whistle/Features/Favorites/view/FavoritesTableViewController.swift
+++ Whistle/Features/Favorites/view/FavoritesTableViewController.swift
@@ -7,83 +7,109 @@
 
 import UIKit
 
+protocol FavoritesViewProtocol: AnyObject {
+    func reloadFavoritesData()
+    func showLoading()
+    func hideLoading()
+}
+
 class FavoritesTableViewController: UITableViewController {
 
+    private var presenter: FavoritesPresenterProtocol!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+   
+        setupFullScreenBackground()
+        setupTableView()
+        
+        presenter = FavoritesPresenter(view: self)
+        presenter.viewDidLoad()
+        
+        self.title = "Favorites"
+        self.navigationController?.navigationBar.prefersLargeTitles = true
     }
-
-    // MARK: - Table view data source
-
-    override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
-        return 0
+    
+    private func setupFullScreenBackground() {
+        let backgroundImageView = UIImageView()
+        backgroundImageView.image = UIImage(named: "screen_bg")
+        backgroundImageView.contentMode = .scaleAspectFill
+        backgroundImageView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.backgroundView = backgroundImageView
     }
+    
+    private func setupTableView() {
+        tableView.backgroundColor = .clear
+        tableView.showsVerticalScrollIndicator = false
+        tableView.separatorStyle = .none
+    }
+}
 
+
+extension FavoritesTableViewController {
+        override func numberOfSections(in tableView: UITableView) -> Int {
+        return presenter?.numberOfFavorites ?? 0
+    }
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 1
     }
-
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FavoritesTableViewCell", for: indexPath) as! FavoritesTableViewCell
+        
+        
         return cell
     }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+    
+    override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 0
     }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
+    
+    override func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let headerView = UIView()
+        headerView.backgroundColor = .clear
+        return headerView
     }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        presenter?.didSelectFavorite(at: indexPath.section)
     }
-    */
+    
 
-    /*
-    // MARK: - Navigation
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+            
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let deleteAction = UIContextualAction(style: .destructive, title: "Delete") { [weak self] (action, view, completionHandler) in
+                
+        //  self?.presenter?.didRemoveFavorite(at: indexPath.section)
+            tableView.deleteSections(IndexSet(integer: indexPath.section), with: .fade)
+            completionHandler(true)
+        }
+            
+        deleteAction.image = UIImage(systemName: "trash.fill")
+        deleteAction.backgroundColor = .systemRed
+            
+        let configuration = UISwipeActionsConfiguration(actions: [deleteAction])
+            
+        configuration.performsFirstActionWithFullSwipe = false
+        
+        return configuration
     }
-    */
+}
 
+extension FavoritesTableViewController: FavoritesViewProtocol {
+    func reloadFavoritesData() {
+        DispatchQueue.main.async { [weak self] in
+            self?.tableView.reloadData()
+        }
+    }
+    
+    func showLoading() { print("Favorites Loading...") }
+    func hideLoading() { print("Favorites Loaded.") }
 }
